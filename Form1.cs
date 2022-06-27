1c0be24e0bdafdd866fac86547d22ead59558cf6
lucasrafaeldesouza <faelsouza.rsg@hotmail.com>
2022-06-27 09:07:01 -0300

    first commit

---
+++ Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace telas
+{
+    public partial class Form1 : Form
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        public Form1()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Text = "Form1";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,88 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Drawing;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Threading;
+using System.IO;
+
+namespace ExemploTela
+{
+    static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            //Application.SetHighDpiMode(HighDpiMode.SystemAware);
+            Application.EnableVisualStyles();
+            //Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Login());
+        }
+    }
+
+    public class Login : Form
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        Label lblUser;
+        Label lblPass;
+
+        TextBox txtUser;
+        TextBox txtPass;
+
+        Button btnConfirm;
+        Button btnCancel;
+
+        public Login()
+        {
+            this.lblUser = new Label();
+            this.lblUser.Text = "Usuário";
+            this.lblUser.Location = new Point(120, 20);
+
+            this.lblPass = new Label();
+            this.lblPass.Text = "Senha";
+            this.lblPass.Location = new Point(120, 80);
+
+            this.txtUser = new TextBox();
+            this.txtUser.Location = new Point(10, 50);
+            this.txtUser.Size = new Size(280, 30);
+
+            this.txtPass = new TextBox();
+            this.txtPass.Location = new Point(10, 110);
+            this.txtPass.Size = new Size(280, 30);
+            this.txtPass.PasswordChar = '*';
+
+            this.btnConfirm = new Button();
+            this.btnConfirm.Text = "Confirmar";
+            this.btnConfirm.Location = new Point(100, 180);
+            this.btnConfirm.Size = new Size(80, 30);
+            //this.btnConfirm.Click += new EventHandler(this.handleConfirmClick);
+
+            this.btnCancel = new Button();
+            this.btnCancel.Text = "Cancelar";
+            this.btnCancel.Location = new Point(100, 220);
+            this.btnCancel.Size = new Size(80, 30);
+            //this.btnCancel.Click += new EventHandler(this.handleCancelClick);
+
+            this.Controls.Add(this.lblUser);
+            this.Controls.Add(this.lblPass);
+
+            this.Controls.Add(this.txtUser);
+            this.Controls.Add(this.txtPass);
+
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnConfirm);
+
+            this.components = new System.ComponentModel.Container();
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1380, 900);
+            this.Text = "Login";
+            this.StartPosition = FormStartPosition.CenterScreen;
+        }
+    }
+}
