d72dd966ce23f13bc58b9b20b2950181cecacfd2
lucasrafaeldesouza <faelsouza.rsg@hotmail.com>
2022-06-28 20:48:14 -0300

    atualizações

--- Program.cs
+++ Program.cs
@@ -34,24 +34,25 @@ namespace ExemploTela
         TextBox txtPass;
         Button btnConfirm;
         Button btnCancel;
+        PictureBox pbImagem;
 
         public Login()
         {
             this.lblUser = new Label();
             this.lblUser.Text = "Usuário";
-            this.lblUser.Location = new Point(700, 52);
+            this.lblUser.Location = new Point(700, 350);
             this.lblUser.Size = new Size(50, 30);
 
             this.lblPass = new Label();
             this.lblPass.Text = "Senha";
-            this.lblPass.Location = new Point(700, 112);
+            this.lblPass.Location = new Point(700, 400);
 
             this.txtUser = new TextBox();
-            this.txtUser.Location = new Point(800, 50);
+            this.txtUser.Location = new Point(800, 350);
             this.txtUser.Size = new Size(280, 30);
 
             this.txtPass = new TextBox();
-            this.txtPass.Location = new Point(800, 110);
+            this.txtPass.Location = new Point(800, 400);
             this.txtPass.Size = new Size(280, 30);
             this.txtPass.PasswordChar = '*';
 
@@ -67,6 +68,14 @@ namespace ExemploTela
             this.btnCancel.Location = new Point(840, 180);
             this.btnCancel.Size = new Size(80, 30);
             //this.btnCancel.Click += new EventHandler(this.handleCancelClick);
+            //FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+
+            pbImagem = new PictureBox();
+			pbImagem.Size = new Size(100,100);
+			pbImagem.Location = new Point(870,240);
+			pbImagem.ClientSize = new Size(100,100);
+			pbImagem.Load("image.png");
+			pbImagem.SizeMode = PictureBoxSizeMode.Zoom;
 
             this.Controls.Add(this.lblUser);
             this.Controls.Add(this.lblPass);
@@ -76,14 +85,18 @@ namespace ExemploTela
 
             this.Controls.Add(this.btnCancel);
             this.Controls.Add(this.btnConfirm);
+
+            this.Controls.Add(pbImagem);
            
 
             this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.WindowState = FormWindowState.Maximized;
-            this.FormBorderStyle = FormBorderStyle.None;
+            //this.FormBorderStyle = FormBorderStyle.None;
             this.Text = "Login";
             this.StartPosition = FormStartPosition.CenterScreen;
         }
     }
 }
+
+
