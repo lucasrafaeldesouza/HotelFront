2a5925b4f2f838c547109fdb87221b099fd3259e
lucasrafaeldesouza <faelsouza.rsg@hotmail.com>
2022-06-28 21:45:28 -0300

    Tela de login finalizada

--- Program.cs
+++ Program.cs
@@ -35,17 +35,32 @@ namespace ExemploTela
         Button btnConfirm;
         Button btnCancel;
         PictureBox pbImagem;
+        Label lblCadastro;
 
         public Login()
         {
             this.lblUser = new Label();
             this.lblUser.Text = "Usuário";
             this.lblUser.Location = new Point(700, 350);
-            this.lblUser.Size = new Size(50, 30);
+            this.lblUser.Size = new Size(100, 30);
+            this.lblUser.ForeColor = Color.Green;
+            this.lblUser.Font = new Font("Calibri", 15);
 
             this.lblPass = new Label();
             this.lblPass.Text = "Senha";
             this.lblPass.Location = new Point(700, 400);
+            this.lblPass.ForeColor = Color.Green;
+            this.lblPass.Font = new Font("Calibri", 15);
+
+            this.lblCadastro = new Label();
+            this.lblCadastro.Text = "Realizar Cadastro";
+            this.lblCadastro.Location = new Point(880, 500);
+            this.lblCadastro.Size = new Size(300, 30);
+            this.lblCadastro.ForeColor = Color.Green;
+            //this.lblCadastro.Font = new Font("Calibri", 15);
+            this.lblCadastro.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCadastro.Click += new EventHandler(this.handleConfirmClickCadastro);
+
 
             this.txtUser = new TextBox();
             this.txtUser.Location = new Point(800, 350);
@@ -56,11 +71,12 @@ namespace ExemploTela
             this.txtPass.Size = new Size(280, 30);
             this.txtPass.PasswordChar = '*';
 
+
             this.btnConfirm = new Button();
             this.btnConfirm.Text = "Confirmar";
             this.btnConfirm.Location = new Point(945, 450);
             this.btnConfirm.Size = new Size(80, 30);
-            
+
             //this.btnConfirm.Click += new EventHandler(this.handleConfirmClick);
             this.btnCancel = new Button();
             this.btnCancel.Text = "Cancelar";
@@ -70,14 +86,15 @@ namespace ExemploTela
             //FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
 
             pbImagem = new PictureBox();
-			pbImagem.Size = new Size(150,150);
-			pbImagem.Location = new Point(860,180);
-			pbImagem.ClientSize = new Size(150,150);
-			pbImagem.Load("image.png");
-			pbImagem.SizeMode = PictureBoxSizeMode.Zoom;
+            pbImagem.Size = new Size(150, 150);
+            pbImagem.Location = new Point(860, 180);
+            pbImagem.ClientSize = new Size(150, 150);
+            pbImagem.Load("image.png");
+            pbImagem.SizeMode = PictureBoxSizeMode.Zoom;
 
             this.Controls.Add(this.lblUser);
             this.Controls.Add(this.lblPass);
+            this.Controls.Add(this.lblCadastro);
 
             this.Controls.Add(this.txtUser);
             this.Controls.Add(this.txtPass);
@@ -86,7 +103,7 @@ namespace ExemploTela
             this.Controls.Add(this.btnConfirm);
 
             this.Controls.Add(pbImagem);
-           
+
 
             this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
@@ -94,6 +111,11 @@ namespace ExemploTela
             //this.FormBorderStyle = FormBorderStyle.None;
             this.Text = "Login";
             this.StartPosition = FormStartPosition.CenterScreen;
+
+        }
+        private void handleConfirmClickCadastro(object sender, EventArgs e)
+        {
+
         }
     }
 }
